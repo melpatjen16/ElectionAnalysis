ea24c1140e6e4c6fa0b9ab5a7a55bb76ad3a4650
melpatjen16 <mp.jenkins16@gmail.com>
2022-07-25 13:41:41 -0400

    Commit pypoll.py changes

--- PyPoll.py
+++ PyPoll.py
@@ -22,6 +22,7 @@ candidate_options = []
 candidate_votes = {}
 
 #6 Winning Candidate and Winning Count Tracker
+
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
@@ -73,26 +74,23 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(election_results)
 # 1. Iterate through the candidate list.
     for candidate_name in candidate_votes:
-    # 2. Retrieve vote count of a candidate.
+    # Retrieve vote count of a candidate.
         votes = candidate_votes[candidate_name]
-    # 3. Calculate the percentage of votes.
+    # Calculate the percentage of votes.
         vote_percentage = float(votes) / float(total_votes) * 100
-        candidate_results = (
-            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-    # Print each candidate's voter count and percentage to the terminal.
-        print(candidate_results)
-        #  Save the candidate results to our text file.
-        txt_file.write(candidate_results)
+    #  To do: print out each candidate's name, vote count, and percentage of
+    # votes to the terminal.
 
     # Determine winning vote count and candidate
-# 1. Determine if the votes are greater than the winning count.
+    # Determine if the votes is greater than the winning count.
     if (votes > winning_count) and (vote_percentage > winning_percentage):
-     # 2. If true then set winning_count = votes and winning_percent =
-     # vote_percentage.
-            winning_count = votes
-            winning_candidate = candidate_name
-            winning_percentage = vote_percentage
+         # If true then set winning_count = votes and winning_percent =
+         # vote_percentage.
+         winning_count = votes
+         winning_percentage = vote_percentage
+         # And, set the winning_candidate equal to the candidate's name.
+         winning_candidate = candidate_name
 
 # To do: print out each candidate's name, vote count, and percentage of
 # votes to the terminal.
